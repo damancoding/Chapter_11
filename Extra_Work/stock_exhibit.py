2898f21db331528e3638f29899f678e7471c5587
Amanda M <156499790+damancoding@users.noreply.github.com>
2024-04-23 18:44:35 -0400

    Update stock_exhibit.py
    removed os -> import sys, moved main() to the bottom, trans_text is str.upper, stockName is str.upper, viewing() simplified, updating() added, deleting() added, notes removed, crud note added

--- Extra_Work/stock_exhibit.py
+++ Extra_Work/stock_exhibit.py
@@ -1,74 +1,46 @@
+# Amanda Mam.
+# 4/23/2024
+# Final Project draft
 import sqlite3
-import os
 import datetime
 
+
+# menu is c.r.u.d or  Create, Read, Update, Delete
 # create a menu so that the user can: choose to add or delete an entry from the database, review the entries, or find the total stocks bought/sold by this user/other users.
 
 conn = sqlite3.connect('shurl_base.db')
 
 data = conn.cursor()
 
-
-
-
 data.execute('''CREATE TABLE IF NOT EXISTS stocks
              (date text, trans text, symbol text, qty real, price real)''')
 
-menu = [
-    "a. Insert stock entry", 
-    "b. View Total Entries", 
-    "c. Search for an Entry", 
-    "d. Delete an Entry",
-    "e. Exit"
-]
-print(menu)
-q = input("What would you like to do?")
-print(q)
-
-def catchselect(selection:str):
-    match selection:
-        case "a":
-            buy_stock()
-        case "b":
-            viewing()
-        case "c":
-            ...
-        case "d":
-            ...
-        case "e":
-            quit
-        case default:
-            print("Please choose an option.")
-            q
-
 conn.commit()
+
 def buy_stock():
-    date_text = input('Enter date in YYYY-MM-DD format: ')                                       # I haven't figured out how to add the dashes (////-//-//) so I'll cut out the spaces
+    date_text = input('Enter date in YYYY-MM-DD format: ')
     year, month, day = map(int, date_text.split('-'))
-    date1 = datetime.date(year, month, day)
-    if len(date_text) == 10 and date_text[5] == "-" and date_text[7] == "-":
-        date_text = True
-    else:
-        date_text = False
-        print("Please enter the date in that exact format.")
+
+    if len(date_text) != 10:
+        print("Invalid entry.2")
         date_text
 
-    if date1 == str:                                                                             # Validation if user inputs a str
-        print("Invalid entry.")
-        date_text = int(input("Enter date, YYYY-MM-DD: "))
+    trans_text = str.upper(input("Enter whether you are buying or selling this stock: "))
+    if trans_text == "BUY" or "SELL":
+        print("Valid Entry.")
+        pass
 
-    trans_text = input("Enter whether you are BUYing or SELLing this stock: ").upper()
-    if trans_text != "BUY" or "SELL":                                                            # User has to input action as uppercase to stand out in database
-        print("Invalid entry. Either BUY or SELL.")
-        trans_text = input("Enter whether you are BUYing or SELLing this stock: ")
-
-
-    stockName = input("Enter the stock name abbreviations: ")                                    # The maximum is at 9 because of stocks like NYSE:NEE and others in New York
+    stockName = str.upper(input("Enter the stock name abbreviations: "))                          # The maximum is at 9 because of stocks like NYSE:NEE and others based in New York
     if len(stockName) < 1 or len(stockName) > 9:
-        print("Invalid entry.")
+        print("Invalid entry.1")
         stockName = input("Enter the stock name abbreviations: ")
+    elif stockName != str:
+        print("Invalid entry.2")
+        stockName = input("Enter the stock name abbreviations: ")
+    elif stockName[5] == ":":                                                                    # This pass is specifically for the semicolons in NYSE based companies           
+        pass
 
-    qty_real = int(input("Enter how many stocks to be bought: "))                                # The maximum is at 1000 because this is a test of the database working properly. Later, I will add menu and option to delete previous entries.
+    qty_real = int(input("Enter how many stocks to be bought: "))
     if qty_real < 1 or qty_real > 1000:
         print("Invalid entry.")
         qty_real = int(input("Enter how many stocks to be bought: "))
@@ -84,75 +56,133 @@ def buy_stock():
 
     print(f"The stock you entered was {stockName}, you are {trans_text}ing {qty_real} stocks. Each stock costs {price_real}.")
     print("*********************************************************************************")
-    data.execute(f"INSERT INTO stocks VALUES ('{date1}','{trans_text}','{stockName}',{qty_real},{price_real})")
-
-
-def viewing():
-    men_view = [
-        "a. stockName",
-        "b. Sale",
-        "c. price",
-        "d. Sale date"
-    ]
-    view = input("What do you want to view in the database? ")
-    if view == "a" or "A":
-        view_n = input("Name: ")
-        data.execute(f'SELECT {view_n} FROM stocks')
-        print(data.fetchall())
-        conn.commit()
-        conn.close()
-    elif view == "b" or "B":
-	    view_p = input ("Price range: ")
-		    num_1 = float(input("Enter the first price:"))
-			num_2 = float(input("Enter the first price:"))
-                def validate_data(record):
-                    if view_p != isinstance(record):
-                        return False, "Invalid record format."
-                    else:
-                        continue
-			    price_r = range(num_1,num_2)
-			    data.execute(f'SELECT {price_r} FROM stocks')
-			    print(data.fetchall())
-	elif view == "c" or "C":
-		 view_q = input ("Time frame: ")
-			 time_1 = float(input("Enter first time frame: "))
-			 time_2 = float(input("Enter the second time frame: "))
-			 if time_1 != #not on the list/in the database or time_2 != not on the list/in the database:
-				 
-    
-
+    data.execute(f"INSERT INTO stocks VALUES ('{date_text}','{trans_text}','{stockName}',{qty_real},{price_real})")
+    conn.commit()
     for row in data.fetchall():
         print(row)
+    main()
 
-menq = [
+def viewing():
+    data.execute('SELECT * FROM stocks')
+    for row in data.fetchall():
+        print(row)
+    main()
+
+def updating():                                                                                 # I need to work on everything in this section.
+    menq = [
         "a. Entry Date",
-        "b. Sale",
+        "b. Trades",
         "c. Name of stock",
-        "d. cost range."
-        "e. Homepage"
-]
-def searching():
+        "d. Homepage"
+    ]
+    print(menq)
     look = input("What do you want to search under? ")
     def lookselect(selection:str):
         match selection:
             case "a":
-                time_r = float(input("Please input a range to under: "))                        # I intend to show the list by range via time frame vs YYYY for ease of access for the user and my laptop. 
-                ...
-                data.execute('SELECT date_text FROM stocks')
+                mm = [('January' or 'january'), ('February' or 'february'), ('March' or 'march'), ('April' or 'april'), ('May'), ('June' or 'june'), ('August'), ('September' or 'september'), ('October'), ('November' or 'november'), ('December')]
+                time_r = float(input("Choose year, month, or week to enter: "))
+                if time_r == "year":
+                    time_s = int(input("Enter year: "))
+                    if time_s in range(2023, 2025):
+                        print(data.execute(f'SELECT {time_s} FROM date_text'))
+                    else:
+                        print("Entry not found. Please enter another year.")
+                        time_s = int(input("Enter year: "))
+                elif time_r == "month":
+                    for time_s in range(1,12) or mm:
+                        if time_s is range(1,12) or mm:
+                            for i, item in enumerate(time_s):
+                                mm_sql = 'SELECT time_s FROM date_text'
+                                data.execute(mm_sql)
+                                mmresult = data.fetchall()
+                                for x in mmresult:
+                                    print(x)
+                                    updating()
+                        else:
+                            print("No items in that month.")
+                            mm
+                            time_r
+                            updating()
+                elif time_r == "week":
+                    mm
+                    p_month = input("Please enter a month to search under: ")
+                    men_w = men_w()
+                    while True:
+                        print("1. Week 1")
+                        print("2. Week 2")
+                        print("3. Week 3")
+                        print("4. Week 4")                    
+                        p_week = input("Please whick week to search under: ")
+                        q_wk1 = (1,7)
+                        q_wk2 = (8,14)
+                        q_wk3 = (15,21)
+                        q_wk4 = (22,30)
+                        if p_week == 1:
+                            data.execute(f'SELECT * FROM date_text where {p_week} == {q_wk1}')
+                        elif p_week == 2:
+                            data.execute(f'SELECT * FROM date_text where {p_week} == {q_wk2}')
+                        elif p_week == 3:
+                            data.execute(f'SELECT * FROM date_text where {p_week} == {q_wk3}')
+                        elif p_week == 4:
+                            data.execute(f'SELECT * FROM date_text where {p_week} == {q_wk4}')
+                        else:
+                            print("Please choose an option from Menu.")
             case "b":
-                ...
-                data.execute('SELECT trans_text FROM stocks')
+                trades_q = input("Did you buy or sell item: ")
+                sql = 'SELECT trades_q FROM trans_text'
+                data.execute(sql)
+                myresult = data.fetchall()
+                for x in myresult:
+                    print(x)
+                    updating()
             case "c":
                 name_r = input("Please enter the name of the stock: ")
                 if len(name_r) < 1 or len(name_r) > 9:
                     print("Invalid entry.")
                     name_r                                                                      # I'm not sure if the stock name on file will be retrieved if the variable used here is under another name.
-                data.execute('SELECT name_r FROM stocks')                                       # I'll use "temp" and swap the 2 variable values (unsure if swapping works with str)
+                else:
+                    data.execute('SELECT name_r FROM stocks')
+                    updating()
             case "d":
-                ...                                                                             # Same situation as the time_r variable, ease of access on user and my laptop
-            case "e":
-                ...
+                main()
             case default:
                 print("Please choose an option.")
-    data.execute('SELECT * FROM stocks')
+                updating()
+
+def deleting():                                                                                 # I need to work on this after updating()
+    print("deleting...")
+    main()
+
+def main():
+    menu = [
+        "a. Create a stock entry", 
+        "b. View Total Entries", 
+        "c. Update an Entry", 
+        "d. Delete an Entry",
+        "e. Exit"
+        ]
+    print(menu)
+    q = input("What would you like to do? ")
+
+    def catchselect(selection:str):
+        match selection:
+            case "a":
+                buy_stock()
+            case "b":
+                viewing()
+            case "c":
+                updating()
+            case "d":
+                deleting()
+            case "e":
+                quit
+            case default:
+                print("Please choose an option.")
+                print(menu)
+                q = input("What would you like to do? ")
+    catchselect(q)
+
+
+main()
 conn.close()
